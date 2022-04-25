df17208461a499fac052110ee37175edae7dd2f3
anastasia-whitelab <abarkova@whitelabgx.com>
2022-04-25 14:25:17 +0200

    work in progress :construction:

--- backend/text_summarization/compare_summarization_models.py
+++ backend/text_summarization/compare_summarization_models.py
@@ -1,6 +1,8 @@
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import torch
-from ..database_scraper import fetch_list_pmid, fetch_xml, run_xml_download
+import sys
+sys.path.append("C:\\Users\\headl\\Documents\\github projects\\ceebios\\d4g-season-10\\backend\\database_scraper")
+from pubmed_scraper import fetch_list_pmid, fetch_xml, run_xml_download
 
 print(run_xml_download())
 fetch_xml(2356)
