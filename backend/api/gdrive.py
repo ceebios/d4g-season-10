9f8208f84b24e81d2b8a9cf4bebe96d7ff983985
ceebios <nikolay.tchakarov@ceebios.com>
2022-06-14 21:58:48 +0200

    typo

--- backend/api/gdrive.py
+++ backend/api/gdrive.py
@@ -5,5 +5,5 @@ output = 'paragraph_with_txt_embeddings.parquet'
 gdown.download(url, output, quiet=False)
 
 url = 'https://drive.google.com/uc?id=15erTN3Fqcr2VlaSmRPClFf0B5-EUuWxg'
-output = 'processed_database_parquet_ceebios_plos_database_parquet_20220613.parque'
+output = 'processed_database_parquet_ceebios_plos_database_parquet_20220613.parquet'
 gdown.download(url, output, quiet=False)
