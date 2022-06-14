530fbf8e43d54e915b2f6291966f56dadb8f2d0a
ceebios <nikolay.tchakarov@ceebios.com>
2022-06-14 21:12:11 +0200

    download

---
+++ backend/api/gdrive.py
@@ -0,0 +1,9 @@
+import gdown
+
+url = 'https://drive.google.com/uc?id=1dZ4Ob9BGKCEmIziyCbN3ba1nmUHMBkfN'
+output = 'paragraph_with_txt_embeddings.parquet'
+gdown.download(url, output, quiet=False)
+
+url = 'https://drive.google.com/uc?id=15erTN3Fqcr2VlaSmRPClFf0B5-EUuWxg'
+output = 'processed_database_parquet_ceebios_plos_database_parquet_20220613.parque'
+gdown.download(url, output, quiet=False)
